a83dd23d1b420967386c04b4457b0d8d38ff9089
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-10-09 20:18:37 +0200

    First commit

---
+++ Demo/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    [Serializable]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Id = 1,
+                FirstName = "Mark",
+                LastName = "Twain",
+                Gender = "Male",
+                PhoneNumber = "+358597545",
+                Email = "marktwain",
+                City = "New York",
+                Country = "USA"
+            };
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"D:\Projects\ConsoleApp\Example.txt", FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, employee);
+            stream.Close();
+
+            stream = new FileStream(@"D:\Projects\ConsoleApp\Example.txt", FileMode.Open, FileAccess.Read);
+            Employee employeeNew = (Employee)formatter.Deserialize(stream);
+
+            Console.WriteLine(employeeNew.Id);
+            Console.WriteLine(employeeNew.FirstName);
+            Console.WriteLine(employeeNew.LastName);
+            Console.WriteLine(employeeNew.Gender);
+            Console.WriteLine(employeeNew.PhoneNumber);
+            Console.WriteLine(employeeNew.Email);
+            Console.WriteLine(employeeNew.City);
+            Console.WriteLine(employeeNew.Country);
+            Console.ReadLine();
+        }
+    }
+}
